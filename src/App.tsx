bbd0fc5c68acbad6becd32b4a8359df1248950db
mmraju77 <100967039+mmraju77@users.noreply.github.com>
2026-07-22 20:17:38 +0530

    feat: add support for additional insurance providers
    Expand the insurance partner selection logic and affiliate redirection to include Allianz, Swiss Re, AIA, Chubb, and AXA.

--- src/App.tsx
+++ src/App.tsx
@@ -4397,6 +4397,16 @@ export default function App() {
                                   onClick={() => {
                                     if (partner.name.includes('Bupa')) {
                                       setSelectedInsuranceProvider('Bupa');
+                                    } else if (partner.name.includes('Allianz')) {
+                                      setSelectedInsuranceProvider('Allianz');
+                                    } else if (partner.name.includes('Swiss Re')) {
+                                      setSelectedInsuranceProvider('SwissRe');
+                                    } else if (partner.name.includes('AIA')) {
+                                      setSelectedInsuranceProvider('AIA');
+                                    } else if (partner.name.includes('Chubb')) {
+                                      setSelectedInsuranceProvider('Chubb');
+                                    } else if (partner.name.includes('AXA')) {
+                                      setSelectedInsuranceProvider('AXA');
                                     } else {
                                       setSelectedInsuranceProvider('Cigna');
                                     }
@@ -6921,6 +6931,16 @@ export default function App() {
                     // Affiliate Auto-Redirect
                     if (selectedInsuranceProvider === 'Bupa') {
                       window.open('https://www.bupaglobal.com/?aff_id=YOUR_FUTURE_ID_HERE', '_blank');
+                    } else if (selectedInsuranceProvider === 'Allianz') {
+                      window.open('https://www.allianz.com/?aff_id=YOUR_FUTURE_ID_HERE', '_blank');
+                    } else if (selectedInsuranceProvider === 'SwissRe') {
+                      window.open('https://www.swissre.com/?aff_id=YOUR_FUTURE_ID_HERE', '_blank');
+                    } else if (selectedInsuranceProvider === 'AIA') {
+                      window.open('https://www.aia.com/?aff_id=YOUR_FUTURE_ID_HERE', '_blank');
+                    } else if (selectedInsuranceProvider === 'Chubb') {
+                      window.open('https://www.chubb.com/?aff_id=YOUR_FUTURE_ID_HERE', '_blank');
+                    } else if (selectedInsuranceProvider === 'AXA') {
+                      window.open('https://www.axa.com/?aff_id=YOUR_FUTURE_ID_HERE', '_blank');
                     } else {
                       window.open('https://www.cignaglobal.com/?aff_id=YOUR_FUTURE_ID_HERE', '_blank');
                     }
